6dec8f548aaa2b0beb66bacffa9e22bcd3aaed85
aaronlelevier <aaron.lelevier@gmail.com>
2022-06-04 10:51:22 -0700

    liam.py boto3 server

--- liam.py
+++ liam.py
@@ -1,29 +1,45 @@
-# import boto3
-# from flask import Flask
+import json
 
-# app = Flask(__name__)
+import boto3
+from werkzeug.exceptions import HTTPException
+from werkzeug.routing import Map, Rule
+from werkzeug.serving import run_simple
+from werkzeug.wrappers import Request, Response
 
-# @app.route("/<service>/<method>/")
-# def entrypoint(service, method):
-#     return getattr(boto3.client(service), method)()
+
+class App:
+
+    def __init__(self):
+        self.url_map = Map([
+            Rule('/<service>/<method>', endpoint='boto_request'),
+        ])
+
+    def __call__(self, environ, start_response):
+        return self.wsgi_app(environ, start_response)
+
+    def wsgi_app(self, environ, start_response):
+        request = Request(environ)
+        response = self.dispatch_request(request)
+        return response(environ, start_response)
+
+    def dispatch_request(self, request):
+        """
+        Dispatcher with 'on_<endpoint>' which can match any endpoint
+        configured in 'self.url_map'
+        """
+        adapter = self.url_map.bind_to_environ(request.environ)
+        try:
+            endpoint, values = adapter.match()
+            return getattr(self, f'on_{endpoint}')(request, **values)
+        except HTTPException as e:
+            return e
+
+    def on_boto_request(self, _request, service, method):
+        resp = getattr(boto3.client(service), method)()
+        return Response(
+            json.dumps(resp, default=str)
+        )
 
 
-import multiprocessing
-from werkzeug import Request, Response, run_simple
-
-def get_token(q: multiprocessing.Queue) -> None:
-    @Request.application
-    def app(request: Request) -> Response:
-        q.put(request.args["token"])
-        return Response("", 204)
-
-    run_simple("localhost", 5000, app)
-
-if __name__ == "__main__":
-    q = multiprocessing.Queue()
-    p = multiprocessing.Process(target=get_token, args=(q,))
-    p.start()
-    print("waiting")
-    token = q.get(block=True)
-    p.terminate()
-    print(token)
+if __name__ == '__main__':
+    run_simple('127.0.0.1', 5000, App(), use_debugger=True, use_reloader=True)
